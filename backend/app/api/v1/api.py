b3525605e3624539627b3234e1e5628b3b63527c
RodiSales87 <rodisales23@gmail.com>
2026-05-11 18:29:07 -0300

    [feat/category-api]: add category router in main index

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,12 +1,11 @@
 # app/api/v1/api.py
 from fastapi import APIRouter
 
-# Importamos os módulos de cada domínio
-from app.api.v1 import clients, tickets, products
+from app.api.v1 import clients, tickets, products, category
 
 api_router = APIRouter()
 
 api_router.include_router(clients.router, prefix="/clients", tags=["Clients"])
 api_router.include_router(tickets.router, prefix="/tickets", tags=["Tickets"])
-
 api_router.include_router(products.router, prefix="/products", tags=["Products"])
+api_router.include_router(category.router, prefix="/categories", tags=["categories"])

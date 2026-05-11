a7522a8f981cc694b31ad1228e15a214f1257f6b
RodiSales87 <rodisales23@gmail.com>
2026-05-11 18:12:38 -0300

    [feat/dashboard-api]: add dashboard router in main index

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,12 +1,10 @@
-# app/api/v1/api.py
 from fastapi import APIRouter
 
-# Importamos os módulos de cada domínio
-from app.api.v1 import clients, tickets, products
+from app.api.v1 import clients, tickets, products, dashboard
 
 api_router = APIRouter()
 
 api_router.include_router(clients.router, prefix="/clients", tags=["Clients"])
 api_router.include_router(tickets.router, prefix="/tickets", tags=["Tickets"])
-
+api_router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 api_router.include_router(products.router, prefix="/products", tags=["Products"])

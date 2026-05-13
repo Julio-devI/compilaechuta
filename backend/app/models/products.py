220595515e3295d69dca205f524aab24e014a07c
PauloHenriqque <hhhenriquepaulo@gmail.com>
2026-05-13 12:37:33 -0300

    fix: update product model to match Gold layer

--- backend/app/models/products.py
+++ backend/app/models/products.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, event, text
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Boolean, DateTime, event, text
 from datetime import datetime
 from app.core.database import Base
+from zoneinfo import ZoneInfo
+
+def get_sp_time():
+    return datetime.now(ZoneInfo("America/Sao_Paulo"))
 
 class Produto(Base):
     __tablename__ = "dim_produto"
@@ -13,9 +17,13 @@ class Produto(Base):
     preco = Column(Float, nullable=True)
     peso_kg = Column(Float, nullable=True)
     estoque_disponivel = Column(Integer, default=0)
-    ativo = Column(Boolean, default=True)
-    precisa_revisao = Column(Boolean, default=False)
-    data_cadastro_produto = Column(DateTime, default=datetime.utcnow)
+    ativo = Column(String, default="Sim")
+    precisa_revisao = Column(String, default="Não")
+    data_cadastro_produto = Column(
+        DateTime(timezone=True), 
+        default=get_sp_time,
+        nullable=True
+    )
     total_pedidos = Column(Integer, default=0)
     receita_total = Column(Float, default=0.0)
     ticket_medio = Column(Float, default=0.0)
@@ -25,3 +33,7 @@ class Produto(Base):
     media_nota_nps = Column(Float, nullable=True)
     pct_recomendacoes_sim = Column(Float, nullable=True)
     total_tickets = Column(Integer, default=0)
+    media_tempo_resolucao_horas = Column(Float, nullable=True)
+    media_nota_suporte = Column(Float, nullable=True)
+    timestamp_ingestion_gold = Column(String, nullable=True)
+    categoria_rel = relationship("Categoria", back_populates="produtos")
